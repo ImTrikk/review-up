b2414bc30e0da3a8ca93eea7285a472faf30f50a
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-11-01 23:58:10 +0800

    added error handling for stronger password

--- api/Controllers/auth.js
+++ api/Controllers/auth.js
@@ -10,179 +10,180 @@ let compressedData;
 
 // login endpoint
 export const login = async (req, res) => {
- try {
-  const { email, password } = req.body;
+	try {
+		const { email, password } = req.body;
 
-  const user = await dbConnection.query(
-   "SELECT * from users where email = $1",
-   [email],
-  );
+		const user = await dbConnection.query(
+			"SELECT * from users where email = $1",
+			[email],
+		);
 
-  const foundUser = user.rows[0];
+		const foundUser = user.rows[0];
 
-  if (user.rows.length === 0) {
-   return res.status(400).json({ message: "User does not exist" });
-  }
-  const result = await dbConnection.query(
-   "SELECT hashed_password from users where email = $1",
-   [email],
-  );
-  const { hashed_password } = result.rows[0];
-  const validatePassword = await bcrypt.compare(password, hashed_password);
+		if (user.rows.length === 0) {
+			return res.status(400).json({ message: "User does not exist" });
+		}
+		const result = await dbConnection.query(
+			"SELECT hashed_password from users where email = $1",
+			[email],
+		);
+		const { hashed_password } = result.rows[0];
+		const validatePassword = await bcrypt.compare(password, hashed_password);
 
-  if (!validatePassword) {
-   return res.status(400).json({ message: "Wrong password" });
-  }
+		if (!validatePassword) {
+			return res.status(400).json({ message: "Wrong password" });
+		}
 
-  const jwtToken = jwtGenerator(user.rows[0].user_id);
-  console.log(jwtToken);
-  res.cookie(jwtToken, "secret");
+		const jwtToken = jwtGenerator(user.rows[0].user_id);
+		console.log(jwtToken);
+		res.cookie(jwtToken, "secret");
 
-  // Call the verification email function with user_id and email
-  const verifyEmail = await OtpVerificationEmail({
-   user_id: user.rows[0].user_id,
-   email: email,
-  });
+		// Call the verification email function with user_id and email
+		const verifyEmail = await OtpVerificationEmail({
+			user_id: user.rows[0].user_id,
+			email: email,
+		});
 
-  //  store data to global
-  globalCode = verifyEmail;
-  compressedData = { foundUser, jwtToken };
+		//  store data to global
+		globalCode = verifyEmail;
+		compressedData = { foundUser, jwtToken };
 
-  return res.status(200).json({ message: "User found" });
- } catch (err) {
-  console.log(err);
- }
+		return res.status(200).json({ message: "User found" });
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 // signup endpoint
 export const signup = async (req, res) => {
- try {
-  const { first_name, last_name, email, phone, password } = req.body;
+	try {
+		const { first_name, last_name, email, phone, password } = req.body;
 
-  // Check if the email already exists
-  const existingEmail = await dbConnection.query(
-   "SELECT * FROM users WHERE email = $1",
-   [email],
-  );
+		// Check if the email already exists
+		const existingEmail = await dbConnection.query(
+			"SELECT * FROM users WHERE email = $1",
+			[email],
+		);
 
-  if (existingEmail.rows.length > 0) {
-   return res.status(400).json({ message: "User already exists!" });
-  }
-  // Insert a new user into the database using parameterized query
-  const newUserQuery = `
+		if (existingEmail.rows.length > 0) {
+			return res.status(400).json({ message: "User already exists!" });
+		}
+		// Insert a new user into the database using parameterized query
+		const newUserQuery = `
       INSERT INTO users (first_name, last_name, email, hashed_password, phone)
       VALUES ($1, $2, $3, $4, $5)
     `;
-  const hashed_password = await bcrypt.hash(password, 10);
-  await dbConnection.query(newUserQuery, [
-   first_name,
-   last_name,
-   email,
-   hashed_password,
-   phone,
-  ]);
+		const hashed_password = await bcrypt.hash(password, 10);
+		await dbConnection.query(newUserQuery, [
+			first_name,
+			last_name,
+			email,
+			hashed_password,
+			phone,
+		]);
 
-  // const jwtToken = jwtGenerator(user.rows[0].user_id);
-  res.status(201).json({ newUserQuery, message: "New user created!" });
- } catch (err) {
-  console.log(err);
-  res.status(500).json({ message: "Internal server error" });
- }
+		// const jwtToken = jwtGenerator(user.rows[0].user_id);
+		res.status(201).json({ newUserQuery, message: "New user created!" });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: "Internal server error" });
+	}
 };
 
 export const TwoFactorAuth = async (req, res) => {
- const { concatenatedCode } = req.body;
- try {
-  if (concatenatedCode === globalCode) {
-   res.cookie(compressedData.jwtToken);
+	const { concatenatedCode } = req.body;
+	try {
+		if (concatenatedCode === globalCode) {
+			res.cookie(compressedData.jwtToken);
 
-   return res
-    .status(200)
-    .json({ compressedData, message: "Verified email account" });
-  } else {
-   return res.status(400).json({ message: "Wrong One time password" });
-  }
- } catch (err) {
-  console.log(err);
- }
+			return res.status(200).json({
+				compressedData,
+				message: "Verified email account",
+			});
+		} else {
+			return res.status(400).json({ message: "Wrong One time password" });
+		}
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 // Verification email function
 const OtpVerificationEmail = async ({ user_id, email }) => {
- try {
-  //  provide a 6 digit code to the client email
-  const otp = `${Math.floor(100000 + Math.random() * 900000)}`;
+	try {
+		//  provide a 6 digit code to the client email
+		const otp = `${Math.floor(100000 + Math.random() * 900000)}`;
 
-  const emailTransporter = nodemailer.createTransport({
-   service: "gmail",
-   host: "smtp.gmail.com",
-   port: 465,
-   secure: true,
-   auth: {
-    user: "reviewupofficial@gmail.com",
-    pass: "hase uosx uyks fhrq",
-   },
-  });
+		const emailTransporter = nodemailer.createTransport({
+			service: "gmail",
+			host: "smtp.gmail.com",
+			port: 465,
+			secure: true,
+			auth: {
+				user: "reviewupofficial@gmail.com",
+				pass: "hase uosx uyks fhrq",
+			},
+		});
 
-  // Send the verification email
-  const mailOptions = {
-   from: "reviewupofficial@gmail.com",
-   to: email,
-   subject: "Your Verification Code",
-   text: `Your verification code is: ${otp}`,
-  };
+		// Send the verification email
+		const mailOptions = {
+			from: "reviewupofficial@gmail.com",
+			to: email,
+			subject: "Your Verification Code",
+			text: `Your verification code is: ${otp}`,
+		};
 
-  //  for checking responses only
-  const info = await emailTransporter.sendMail(mailOptions);
-  console.log("Verification email sent:", info.response);
+		//  for checking responses only
+		const info = await emailTransporter.sendMail(mailOptions);
+		console.log("Verification email sent:", info.response);
 
-  return otp;
- } catch (err) {
-  console.log(err);
- }
+		return otp;
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 export const resendOtp = async (req, res) => {
- try {
-  const { email, password } = req.body;
+	try {
+		const { email, password } = req.body;
 
-  const user = await dbConnection.query(
-   "SELECT * from users where email = $1",
-   [email],
-  );
+		const user = await dbConnection.query(
+			"SELECT * from users where email = $1",
+			[email],
+		);
 
-  const foundUser = user.rows[0];
+		const foundUser = user.rows[0];
 
-  if (user.rows.length === 0) {
-   return res.status(400).json({ message: "User does not exist" });
-  }
-  const result = await dbConnection.query(
-   "SELECT hashed_password from users where email = $1",
-   [email],
-  );
-  const { hashed_password } = result.rows[0];
-  const validatePassword = await bcrypt.compare(password, hashed_password);
+		if (user.rows.length === 0) {
+			return res.status(400).json({ message: "User does not exist" });
+		}
+		const result = await dbConnection.query(
+			"SELECT hashed_password from users where email = $1",
+			[email],
+		);
+		const { hashed_password } = result.rows[0];
+		const validatePassword = await bcrypt.compare(password, hashed_password);
 
-  if (!validatePassword) {
-   return res.status(400).json({ message: "Wrong password" });
-  }
+		if (!validatePassword) {
+			return res.status(400).json({ message: "Wrong password" });
+		}
 
-  const jwtToken = jwtGenerator(user.rows[0].user_id);
-  console.log(jwtToken);
-  res.cookie(jwtToken, "secret");
+		const jwtToken = jwtGenerator(user.rows[0].user_id);
+		console.log(jwtToken);
+		res.cookie(jwtToken, "secret");
 
-  // Call the verification email function with user_id and email
-  const verifyEmail = await OtpVerificationEmail({
-   user_id: user.rows[0].user_id,
-   email: email,
-  });
+		// Call the verification email function with user_id and email
+		const verifyEmail = await OtpVerificationEmail({
+			user_id: user.rows[0].user_id,
+			email: email,
+		});
 
-  //  store data to global
-  globalCode = verifyEmail;
-  compressedData = { foundUser, jwtToken };
+		//  store data to global
+		globalCode = verifyEmail;
+		compressedData = { foundUser, jwtToken };
 
-  return res.status(200).json({ message: "User found" });
- } catch (err) {
-  console.log(err);
- }
+		return res.status(200).json({ message: "User found" });
+	} catch (err) {
+		console.log(err);
+	}
 };
--- src/pages/authentication/Signup.jsx
+++ src/pages/authentication/Signup.jsx
@@ -5,230 +5,273 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export const Signup = () => {
- const [first_name, setFirstName] = useState("");
- const [last_name, setLastName] = useState("");
- const [email, setEmail] = useState("");
- const [password, setPassword] = useState("");
- const [phone, setPhone] = useState("");
- const navLogin = useNavigate();
+	const [first_name, setFirstName] = useState("");
+	const [last_name, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [cPassword, setCPassword] = useState("");
+	const [phone, setPhone] = useState("");
+	const navLogin = useNavigate();
 
- // these states are for the errorHandling of password
+	// these states are for the errorHandling of password
 
- const handleSignupRequest = async (event) => {
-  event.preventDefault();
+	const handleSignupRequest = async (event) => {
+		event.preventDefault();
+		if (password.length <= 12) {
+			toast.error("Password is too short", {
+				position: "top-right",
+				autoClose: 2000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				theme: "light",
+			});
+		} else if (!/[A-Z]/.test(password)) {
+			toast.error("Password must contain upppercase character", {
+				position: "top-right",
+				autoClose: 2000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				theme: "light",
+			});
+		} else if (!/[a-z]/.test(password)) {
+			toast.error("Password must contain lowercase character", {
+				position: "top-right",
+				autoClose: 2000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				theme: "light",
+			});
+		} else if (/[!@#$%^&*]/.test(password)) {
+			toast.error("Password must have symbols (!@#$%^&*)", {
+				position: "top-right",
+				autoClose: 2000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				theme: "light",
+			});
+		} else if (
+			email == "" ||
+			first_name == "" ||
+			last_name == "" ||
+			phone == "" || password == "" || cPassword == ""
+		) {
+			toast.error("Input fields are required", {
+				position: "top-right",
+				autoClose: 2000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				theme: "light",
+			});
+		} else {
+			if (cPassword !== password) {
+				toast.error("Password does not match", {
+					position: "top-right",
+					autoClose: 2000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "light",
+				});
+			} else {
+				try {
+					let response = await fetch(buildUrl("/auth/signup"), {
+						method: "POST",
+						headers: {
+							"Content-type": "application/json",
+						},
+						body: JSON.stringify({
+							first_name,
+							last_name,
+							email,
+							password,
+							phone,
+						}),
+					});
+					if (response.ok) {
+						toast.success("Success creating account!", {
+							position: "top-right",
+							autoClose: 2000,
+							hideProgressBar: false,
+							closeOnClick: true,
+							pauseOnHover: true,
+							draggable: true,
+							progress: undefined,
+							theme: "light",
+						});
+						setTimeout(() => {
+							navLogin("/login");
+						}, 2000);
+					} else {
+						console.log(response.message);
+						toast.error("Error creating account", {
+							position: "top-right",
+							autoClose: 2000,
+							hideProgressBar: false,
+							closeOnClick: true,
+							pauseOnHover: true,
+							draggable: true,
+							progress: undefined,
+							theme: "light",
+						});
+					}
+				} catch (err) {
+					console.log(err);
+				}
+			}
+		}
+	};
 
-  console.log("Handling signup request"); // Add this line to check if the function is called
-  console.log(password);
-
-  if (password.length <= 12) {
-   toast.error("Password is too short", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "light",
-   });
-  } else if (
-   !/[A-Z]/.test(password) ||
-   !/[a-z]/.test(password) ||
-   // /\d/.test(password) ||
-   /[!@#$%^&*]/.test(password)
-  ) {
-   toast.error("Password must contain symbols", {
-    position: "top-right",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "light",
-   });
-  } else {
-   try {
-    let response = await fetch(buildUrl("/auth/signup"), {
-     method: "POST",
-     headers: {
-      "Content-type": "application/json",
-     },
-     body: JSON.stringify({
-      first_name,
-      last_name,
-      email,
-      password,
-      phone,
-     }),
-    });
-    if (response.ok) {
-     console.log(response);
-     toast.success("Success creating account!", {
-      position: "top-right",
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-     });
-     setTimeout(() => {
-      navLogin("/login");
-     }, 2000);
-    } else {
-     console.log(response.message);
-     toast.error("Error creating account", {
-      position: "top-right",
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-     });
-    }
-   } catch (err) {
-    console.log(err);
-   }
-  }
- };
-
- return (
-  <>
-   <div className="bg-primaryColor h-screen">
-    <ToastContainer autoClose={2000} />
-    <div className="flex lg:max-w-7xl mx-20 2xl:mx-auto">
-     <div className="w-[50%] p-10">
-      <Link to="/">
-       <div className="flex items-center gap-2">
-        <img
-         src="/static/images/whitePNGReviewUp.png"
-         alt=""
-         className="w-[20px]"
-        />
-        <h1 className="text-white text-xl font-black">ReviewUp</h1>
-       </div>
-      </Link>
-      <div className="pt-10">
-       <h1 className="text-white text-4xl font-black">
-        Create account to unlock features
-       </h1>
-       <div className="w-44 pt-2">
-        <hr className="text-white border-4" />
-       </div>
-       <div className="pt-5">
-        <p className="text-lg text-white">
-         Learn from peers and share your ideas, reviewers, and other study
-         resources.
-        </p>
-       </div>
-       <div className="pt-5">
-        <div className="text-white">
-         <p className="text-lg font-bold">Tips for creating password</p>
-         <ul className="text-sm font-light pt-2">
-          <ol>1. Password must have 8 minimum characters</ol>
-          <ol>2. Must have at least 1 uppercase letter</ol>
-          <ol>3. Must have at least 1 lowercase letter</ol>
-          <ol>4. Must have at least 1 number from numbers 0-9 </ol>
-         </ul>
-        </div>
-       </div>
-      </div>
-     </div>
-     <div className="w-[50%]">
-      <div className="p-10">
-       <div className="bg-white rounded h-full p-5 shadow-lg">
-        <form>
-         <div className="space-y-4">
-          <div className="grid grid-cols">
-           <label htmlFor="" className="text-sm text-primaryColor font-medium">
-            First name
-           </label>
-           <input
-            type="text"
-            value={first_name}
-            onChange={(e) => setFirstName(e.target.value)}
-            placeholder="firstname"
-            className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
-           />
-          </div>
-          <div className="grid grid-cols">
-           <label htmlFor="" className="text-sm text-primaryColor font-medium">
-            Lastname
-           </label>
-           <input
-            type="text"
-            value={last_name}
-            onChange={(e) => setLastName(e.target.value)}
-            placeholder="lastname"
-            className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
-           />
-          </div>
-          <div className="grid grid-cols">
-           <label htmlFor="" className="text-sm text-primaryColor font-medium">
-            Email
-           </label>
-           <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="email"
-            className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
-           />
-          </div>
-          <div className="grid grid-cols">
-           <label htmlFor="" className="text-sm text-primaryColor font-medium">
-            Phone
-           </label>
-           <input
-            type="number"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            placeholder="phone number"
-            className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
-           />
-          </div>
-          <div className="grid grid-cols">
-           <label htmlFor="" className="text-sm text-primaryColor font-medium">
-            Password
-           </label>
-           <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
-           />
-          </div>
-          {/* <div className="grid grid-cols">
-           <label htmlFor="" className="text-sm text-primaryColor font-medium">
-            Confirm password
-           </label>
-           <input
-            type="text"
-            placeholder="type your password again"
-            className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
-           />
-          </div> */}
-         </div>
-         <div className="pt-10">
-          <button
-           type="submit"
-           onClick={handleSignupRequest}
-           className="bg-primaryColor text-white font-bold h-10 w-full rounded"
-          >
-           Register
-          </button>
-         </div>
-        </form>
-       </div>
-      </div>
-     </div>
-    </div>
-   </div>
-  </>
- );
+	return (
+		<>
+			<div className="bg-primaryColor h-screen">
+				<ToastContainer autoClose={2000} />
+				<div className="flex lg:max-w-7xl mx-20 2xl:mx-auto">
+					<div className="w-[50%] p-10">
+						<Link to="/">
+							<div className="flex items-center gap-2">
+								<img
+									src="/static/images/whitePNGReviewUp.png"
+									alt=""
+									className="w-[20px]"
+								/>
+								<h1 className="text-white text-xl font-black">ReviewUp</h1>
+							</div>
+						</Link>
+						<div className="pt-10">
+							<h1 className="text-white text-4xl font-black">
+								Create account to unlock features
+							</h1>
+							<div className="w-44 pt-2">
+								<hr className="text-white border-4" />
+							</div>
+							<div className="pt-5">
+								<p className="text-lg text-white">
+									Learn from peers and share your ideas, reviewers, and other study
+									resources.
+								</p>
+							</div>
+							<div className="pt-5">
+								<div className="text-white">
+									<p className="text-lg font-bold">Tips for creating password</p>
+									<ul className="text-sm font-light pt-2">
+										<ol>1. Password must have 8 minimum characters</ol>
+										<ol>2. Must have at least 1 uppercase letter</ol>
+										<ol>3. Must have at least 1 lowercase letter</ol>
+										<ol>4. Must have at least 1 number from numbers 0-9 </ol>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="w-[50%]">
+						<div className="p-10">
+							<div className="bg-white rounded h-full p-5 shadow-lg">
+								<form>
+									<div className="space-y-4">
+										<div className="grid grid-cols">
+											<label htmlFor="" className="text-sm text-primaryColor font-medium">
+												First name
+											</label>
+											<input
+												type="text"
+												value={first_name}
+												onChange={(e) => setFirstName(e.target.value)}
+												placeholder="firstname"
+												className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
+											/>
+										</div>
+										<div className="grid grid-cols">
+											<label htmlFor="" className="text-sm text-primaryColor font-medium">
+												Lastname
+											</label>
+											<input
+												type="text"
+												value={last_name}
+												onChange={(e) => setLastName(e.target.value)}
+												placeholder="lastname"
+												className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
+											/>
+										</div>
+										<div className="grid grid-cols">
+											<label htmlFor="" className="text-sm text-primaryColor font-medium">
+												Email
+											</label>
+											<input
+												type="email"
+												value={email}
+												onChange={(e) => setEmail(e.target.value)}
+												placeholder="email"
+												className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
+											/>
+										</div>
+										<div className="grid grid-cols">
+											<label htmlFor="" className="text-sm text-primaryColor font-medium">
+												Phone
+											</label>
+											<input
+												type="number"
+												value={phone}
+												onChange={(e) => setPhone(e.target.value)}
+												placeholder="phone number"
+												className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
+											/>
+										</div>
+										<div className="grid grid-cols">
+											<label htmlFor="" className="text-sm text-primaryColor font-medium">
+												Password
+											</label>
+											<input
+												type="password"
+												value={password}
+												onChange={(e) => setPassword(e.target.value)}
+												placeholder="Password"
+												className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
+											/>
+										</div>
+										<div className="grid grid-cols">
+											<label htmlFor="" className="text-sm text-primaryColor font-medium">
+												Confirm password
+											</label>
+											<input
+												type="text"
+												value={cPassword}
+												onChange={(e) => setCPassword(e.target.value)}
+												placeholder="type your password again"
+												className="border border-gray-200 text-xs font-light h-10 px-2 rounded outline-primaryColor"
+											/>
+										</div>
+									</div>
+									<div className="pt-10">
+										<button
+											type="submit"
+											onClick={handleSignupRequest}
+											className="bg-primaryColor text-white font-bold h-10 w-full rounded">
+											Register
+										</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
