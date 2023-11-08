89ca9518ab44dd2e8897485cd7fc5405886cb9e7
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-11-08 22:42:06 +0800

    added quiz and link initial UI

--- src/components/Modal/LinkResourcesModal.jsx
+++ src/components/Modal/LinkResourcesModal.jsx
@@ -1,13 +1,66 @@
+import { BsFillPlusCircleFill } from "react-icons/bs";
+
 export const LinkResourcesModal = () => {
+	const dataSample = [
+		{
+			id: 1,
+			link:
+				"https://www.figma.com/file/HeBeuTthmI6MdAaOGrQh98/ReviewUP?node-id=184%3A406&mode=dev",
+		},
+		{
+			id: 2,
+			link:
+				"https://www.figma.com/file_1/HeBeuTthmI6MdAaOGrQh98/ReviewUP?node-id=184%3A406&mode=dev",
+		},
+		{
+			id: 3,
+			link:
+				"https://www.figma.com/file_2/HeBeuTthmI6MdAaOGrQh98/ReviewUP?node-id=184%3A406&mode=dev",
+		},
+		{
+			id: 4,
+			link:
+				"https://www.figma.com/file_3/HeBeuTthmI6MdAaOGrQh98/ReviewUP?node-id=184%3A406&mode=dev",
+		},
+	];
+
 	return (
 		<>
 			<div className="pt-8">
 				<div className="border border-primaryColor shadow rounded p-5">
-					<h1 className="text-primaryColor text-xs">
-					Other resources
-					</h1>
+					<h1 className="text-primaryColor text-xs">Other resources</h1>
 					<div>
 						<form action="">
+							<div className="flex gap-6">
+								{/* Button - Add Link  */}
+								<div className="pt-5 ">
+									<div className="flex flex-wrap gap-2">
+										{/* whenever thebutton is pressed, it should render another input field */}
+										<button
+											className="flex justify-center items-center border p-2 text-[15px] gap-2 bg-primaryColor rounded-[5px] px-4">
+											<BsFillPlusCircleFill size={16} style={{ color: "white" }} />
+											<span className="text-xs text-white">Add Link</span>
+										</button>
+									</div>
+								</div>
+							</div>
+							{/* Links Container  */}
+							<div className="py-3 flex flex-col gap-4 ">
+								{dataSample.map((obj) => (
+									<div
+										className="flex items-center right-0 max-w-[950px] h-[60px] bg-white rounded border border-red  border-opacity-10 p-2"
+										key={obj.id}>
+										<p className="w-full flex items-center pl-2 text-gray-400 text-xs ">
+											{obj.link}
+										</p>
+										<a href={obj.link} target="_blank" rel="noopener noreferrer">
+											<button className="bg-indigo-500 text-xs h-8 rounded-[5px] text-white px-5 py-1 ">
+												Link
+											</button>
+										</a>
+									</div>
+								))}
+							</div>
 							<div className="flex justify-end pt-5">
 								<button className="text-xs text-white bg-primaryColor rounded h-8 px-4">
 									Save
--- src/components/Modal/QuizModal.jsx
+++ src/components/Modal/QuizModal.jsx
@@ -1,3 +1,5 @@
+import { BsFillPlusCircleFill } from "react-icons/bs";
+
 export const QuizModal = () => {
 	return (
 		<>
@@ -8,6 +10,41 @@ export const QuizModal = () => {
 					</h1>
 					<div>
 						<form action="">
+							<div className="pt-5">
+								<div className="flex flex-wrap gap-2">
+									<button className="flex justify-center items-center border p-2 text-[15px] gap-2 bg-primaryColor rounded-[5px]">
+										<BsFillPlusCircleFill size={16} style={{ color: "white" }} />
+										<span className="text-white">Question</span>
+									</button>
+								</div>
+								{/* Main Container */}
+								<div className="bg-primaryColor w-full rounded-[10px] mt-5 shadow-lg p-4 text-white">
+									<div>
+										<h3 className="font-bold">Steps</h3>
+										<div className="py-2">
+											<hr className="border-1 border-white"/>
+										</div>
+										<div className="flex items-center gap-2 mt-2">
+											<div className="bg-white rounded-full text-primaryColor inline-block px-[8px] py-[1px] text-sm ">
+												1
+											</div>
+											<p className="text-sm font-medium">Create a question</p>
+										</div>
+										<div className="flex items-center gap-2 mt-2">
+											<div className="bg-white rounded-full text-primaryColor inline-block px-[7px] py-[1px] text-sm ">
+												2
+											</div>
+											<p className="text-sm font-medium">Add answer choices</p>
+										</div>
+										<div className="flex items-center gap-2 mt-2">
+											<div className="bg-white rounded-full text-primaryColor inline-block px-[7px] py-[1px] text-sm ">
+												3
+											</div>
+											<p className="text-sm font-medium">Select correct answer</p>
+										</div>
+									</div>
+								</div>
+							</div>
 							<div className="flex justify-end pt-5">
 								<button className="text-xs text-white bg-primaryColor rounded h-8 px-4">
 									Save
