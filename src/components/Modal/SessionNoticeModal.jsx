7219fa6c4ef79f3f977f6480ca74926757783deb
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-11-16 00:31:52 +0800

    improved modal for session timeout

--- src/components/Modal/SessionNoticeModal.jsx
+++ src/components/Modal/SessionNoticeModal.jsx
@@ -25,7 +25,12 @@ export const SessionNoticeModal = ({ isOpen, onClose }) => {
 			className={`fixed top-0 left-0 w-full h-full flex items-center justify-center ${
 				isOpen ? "" : "hidden"
 			}`}>
-			<div className="border border-gray-100 bg-white w-96 rounded p-5">
+			{/* Blurred background */}
+			<div
+				className="fixed top-0 left-0 w-full h-full backdrop-filter backdrop-blur-sm"
+				style={{ backgroundColor: "rgba(255, 255, 255, 0.8)" }}></div>
+
+			<div className="border border-gray-300 bg-white w-96 rounded p-5 relative">
 				<h1 className="text-lg font-bold text-primaryColor mb-4">
 					Session Timeout
 				</h1>
@@ -33,17 +38,17 @@ export const SessionNoticeModal = ({ isOpen, onClose }) => {
 					<hr />
 				</div>
 				<p>
-					Your session has timeout due to inactivity, please login again to
-					reauthenticates
+					Your session has timed out due to inactivity. Please login again to
+					reauthenticate.
 				</p>
 				<div className="flex justify-end">
 					<button
 						onClick={onClose}
-						className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">
-						Close
+						className="mt-4 bg-blue-500 text-white text-xs px-4 py-2 rounded">
+						Okay
 					</button>
 				</div>
 			</div>
 		</div>
-	);
+	);7
 };
