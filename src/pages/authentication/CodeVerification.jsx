5fb061803cffa476e44c664e72bef8150a9a6c87
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-11-16 08:33:17 +0800

    fixed issues code Verification

--- src/pages/authentication/CodeVerification.jsx
+++ src/pages/authentication/CodeVerification.jsx
@@ -9,149 +9,176 @@ export const CodeVerification = () => {
 	const navigate = useNavigate();
 	const location = useLocation();
 	const userData = location.state ? location.state.userData : null;
-		const reqEndpoint = location.state ? location.state.endpoint : null;
-		const [countdown, setCountdown] = useState(30);
+	const reqEndpoint = location.state ? location.state.endpoint : null;
+	const [countdown, setCountdown] = useState(30);
+	const [attemp, setAttempt] = useState(3);
+	const [retryCountdown, setRetryCountDown] = useState(180);
 
-		const [verificationCode, setVerificationCode] = useState([
-			"",
-			"",
-			"",
-			"",
-			"",
-			"",
-		]);
+	const [verificationCode, setVerificationCode] = useState([
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+	]);
 
-		const loadingBar = useRef(null);
-		const inputRefs = verificationCode.map(() => useRef());
+	const loadingBar = useRef(null);
+	const inputRefs = verificationCode.map(() => useRef());
 
-		const navigator = useNavigate();
+	const navigator = useNavigate();
 
-		const handleInputChange = (index, value) => {
-			setVerificationCode((prevCodes) => {
-				const newVerificationCode = [...prevCodes];
-				newVerificationCode[index] = value;
-				return newVerificationCode;
-			});
+	const handleInputChange = (index, value) => {
+		setVerificationCode((prevCodes) => {
+			const newVerificationCode = [...prevCodes];
+			newVerificationCode[index] = value;
+			return newVerificationCode;
+		});
 
-			// If a single number is entered, move to the next input field
-			if (value.length === 1 && index < inputRefs.length - 1) {
-				inputRefs[index + 1].current.focus();
-			}
-		};
+		// If a single number is entered, move to the next input field
+		if (value.length === 1 && index < inputRefs.length - 1) {
+			inputRefs[index + 1].current.focus();
+		}
+	};
 
-		let concatenatedCode = verificationCode.join("");
+	let concatenatedCode = verificationCode.join("");
 
-		// send data to server side
-		const handleVerifyCode = async () => {
-			try {
-				// this should go to the TwoFactorAtuh endpoint
-				await fetch(buildUrl(`/auth${reqEndpoint}`), {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						userData,
-						concatenatedCode,
-					}),
-				}).then((res) => {
-					if (res.status === 200 || res.status === 201) {
-						if (reqEndpoint === "/signup") {
-							toast.success("Account created, redirecting to login page...");
-							loadingBar.current.continuousStart(60);
+	// send data to server side
+	const handleVerifyCode = async () => {
+		try {
+			// this should go to the TwoFactorAtuh endpoint
+			await fetch(buildUrl(`/auth${reqEndpoint}`), {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					userData,
+					concatenatedCode,
+				}),
+			}).then((res) => {
+				if (res.status === 200 || res.status === 201) {
+					if (reqEndpoint === "/signup") {
+						toast.success("Account created, redirecting to login page...");
+						loadingBar.current.continuousStart(60);
+						setTimeout(() => {
+							loadingBar.current.complete();
+							setTimeout(() => {
+								navigator("/login");
+							}, 1200);
+						}, 1000);
+					} else {
+						return res.json().then((data) => {
+							localStorage.setItem("user_id", data.foundUser.user_id);
+							localStorage.setItem("token", data.jwtToken);
+							localStorage.setItem("first_name", data.foundUser.first_name);
+							localStorage.setItem("last_name", data.foundUser.last_name);
+							localStorage.setItem("email", data.foundUser.email);
+							localStorage.setItem("phone", data.foundUser.phone);
+							toast.success("Verified email, login successful!", {
+								position: "top-right",
+								autoClose: 2000,
+								hideProgressBar: false,
+								closeOnClick: true,
+								pauseOnHover: true,
+								draggable: true,
+								progress: undefined,
+								theme: "light",
+							});
+							loadingBar.current.continuousStart(50);
 							setTimeout(() => {
 								loadingBar.current.complete();
 								setTimeout(() => {
-									navigator("/login");
+									navigator("/dashboard");
 								}, 1200);
 							}, 1000);
-						} else {
-							return res.json().then((data) => {
-								localStorage.setItem("user_id", data.foundUser.user_id);
-								localStorage.setItem("token", data.jwtToken);
-								localStorage.setItem("first_name", data.foundUser.first_name);
-								localStorage.setItem("last_name", data.foundUser.last_name);
-								localStorage.setItem("email", data.foundUser.email);
-								localStorage.setItem("phone", data.foundUser.phone);
-								toast.success("Verified email, login successful!", {
-									position: "top-right",
-									autoClose: 2000,
-									hideProgressBar: false,
-									closeOnClick: true,
-									pauseOnHover: true,
-									draggable: true,
-									progress: undefined,
-									theme: "light",
-								});
-								loadingBar.current.continuousStart(50);
-								setTimeout(() => {
-									loadingBar.current.complete();
-									setTimeout(() => {
-										navigator("/dashboard");
-									}, 1200);
-								}, 1000);
-							});
-						}
-					} else {
-						toast.error("Entered wrong OTP code!");
+						});
+					}
+				} else {
+					setAttempt(attemp - 1);
+					toast.error("Entered wrong OTP code!");
+				}
+			});
+		} catch (err) {
+			console.log(err);
+			toast.error(err);
+		}
+	};
+
+	const handleResendOtp = async () => {
+		setCountdown(30);
+		let reqEndpointOtp;
+		const { email, password } = userData;
+		let requestBody = {};
+		concatenatedCode = "";
+		try {
+			if (reqEndpoint === "/signup") {
+				reqEndpointOtp = "send-otp";
+			} else {
+				reqEndpointOtp = "send-otp-login";
+			}
+
+			// Set the request body based on reqEndpoint
+			if (reqEndpoint === "/signup") {
+				requestBody = userData;
+			}
+
+			await fetch(buildUrl(`/auth/${reqEndpointOtp}`), {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ email, password, requestBody }),
+			}).then((res) => {
+				return res.json().then((data) => {
+					if (res.ok) {
+						toast.info("Code has been sent to your email account!");
 					}
 				});
-			} catch (err) {
-				console.log(err);
-				toast.error(err);
-			}
-		};
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-		const handleResendOtp = async () => {
-			setCountdown(30);
-			let reqEndpointOtp;
-			const { email, password } = userData;
-			let requestBody = {};
-			concatenatedCode = "";
-			try {
-				if (reqEndpoint === "/signup") {
-					reqEndpointOtp = "send-otp";
-				} else {
-					reqEndpointOtp = "send-otp-login";
+	const attemptCount = () => {};
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCountdown((prevCountdown) => {
+				if (prevCountdown === 0) {
+					clearInterval(timer);
+					return 0;
 				}
-
-				// Set the request body based on reqEndpoint
-				if (reqEndpoint === "/signup") {
-					requestBody = userData;
-				}
-
-				await fetch(buildUrl(`/auth/${reqEndpointOtp}`), {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({ email, password, requestBody }),
-				}).then((res) => {
-					return res.json().then((data) => {
-						if (res.ok) {
-							toast.info("Code has been sent to your email account!");
-						}
-					});
-				});
-			} catch (err) {
-				console.log(err);
-			}
+				return prevCountdown - 1;
+			});
+		}, 1000);
+		return () => {
+			clearInterval(timer);
 		};
+	});
 
-		useEffect(() => {
-			const timer = setInterval(() => {
-				setCountdown((prevCountdown) => {
+	useEffect(() => {
+		let timer;
+
+		if (attemp === 0) {
+			timer = setInterval(() => {
+				setRetryCountDown((prevCountdown) => {
 					if (prevCountdown === 0) {
 						clearInterval(timer);
+						setRetryCountDown(180);
+						setAttempt(3);
 						return 0;
 					}
 					return prevCountdown - 1;
 				});
 			}, 1000);
-			return () => {
-				clearInterval(timer);
-			};
-		});
+		}
+
+		return () => {
+			clearInterval(timer);
+		};
+	}, [attemp, retryCountdown]);
 
 	return (
 		<>
@@ -173,7 +200,7 @@ export const CodeVerification = () => {
 									</p>
 								</Link>
 							</div>
-							<div className="w-[500px] bg-white h-[270px] rounded p-5">
+							<div className="w-auto bg-white h-auto rounded p-10">
 								<div>
 									<p className="text-sm font-semibold text-primaryColor">
 										Email verification code
@@ -183,33 +210,56 @@ export const CodeVerification = () => {
 									</p>
 								</div>
 								<div className="flex items-center justify-center pt-10">
-									<div className="flex gap-2">
-										{verificationCode.map((value, index) => (
-											<input
-												key={index}
-												type="text"
-												className="h-14 w-14 rounded bg-white text-lg text-center outline-primaryColor border border-primaryColor"
-												value={value}
-												onChange={(e) => handleInputChange(index, e.target.value)}
-												ref={inputRefs[index]}
-												inputMode="numeric"
-											/>
-										))}
+									<div>
+										<div className="flex gap-2">
+											{verificationCode.map((value, index) => (
+												<input
+													key={index}
+													type="text"
+													className="h-14 w-14 rounded bg-white text-lg text-center outline-primaryColor border border-primaryColor"
+													value={value}
+													onChange={(e) => handleInputChange(index, e.target.value)}
+													ref={inputRefs[index]}
+													inputMode="numeric"
+												/>
+											))}
+										</div>
+										<div className="mt-5">
+											{attemp === 0 ? (
+												<p className="text-xs">Retry in: {retryCountdown}</p>
+											) : (
+												<p className="text-xs">Attemps left: {attemp}</p>
+											)}
+										</div>
 									</div>
 								</div>
-								<div className="flex justify-between items-center pt-16">
+								{attemp === 0 ? (
+									""
+								) : (
+									<div className="flex justify-between items-center mt-5">
+										{countdown === 0 ? (
+											<button
+												onClick={handleResendOtp}
+												className="text-xs text-primaryColor"
+												disabled={countdown > 0 || attemp === 0}>
+												Resend code
+											</button>
+										) : (
+											<button
+												className="text-xs text-primaryColor"
+												disabled={countdown > 0 || attemp === 0}>
+												Resend in {countdown}
+											</button>
+										)}
+
 										<button
-											onClick={handleResendOtp}
-											className="text-xs text-primaryColor"
-											disabled={countdown > 0}>
-											Resend in {countdown}
+											onClick={handleVerifyCode}
+											disabled={attemp === 0}
+											className="bg-primaryColor text-white px-2 h-8 rounded text-xs">
+											Send
 										</button>
-									<button
-										onClick={handleVerifyCode}
-										className="bg-primaryColor text-white px-2 h-8 rounded text-xs">
-										Send
-									</button>
-								</div>
+									</div>
+								)}
 							</div>
 						</div>
 					</div>
