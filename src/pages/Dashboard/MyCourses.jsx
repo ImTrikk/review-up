9a31551e8e46eb1f7cde02b6e71666a0a27f5f3f
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-11-30 19:18:12 +0800

    update

--- src/pages/Dashboard/MyCourses.jsx
+++ src/pages/Dashboard/MyCourses.jsx
@@ -6,42 +6,38 @@ import { ReviewModuleCard } from "../../components/ReviewModuleCard";
 import Header from "../../components/header/Header";
 
 export const MyCourses = () => {
-  const user_id = localStorage.getItem("user_id");
-  const [isEmpty, setIsEmpty] = useState(null);
+	const user_id = localStorage.getItem("user_id");
+	const [isEmpty, setIsEmpty] = useState(null);
 
-  const handleIsEmpty = (value) => {
-    setIsEmpty(value.length === 0);
-  };
+	const handleIsEmpty = (value) => {
+		setIsEmpty(value.length === 0);
+	};
 
-  return (
-    <>
-      <div className="bg-[#f2f2f2] h-screen">
-        <SideBar />
-        <Header
-          title={"MyCourse"}
-          description={
-            "Keep track on the save reviewers to keep learning and improving"
-          }
-        />
-        <div className="ml-[210px]">
-          <div className="p-8">
-            <div className="pt-5">
-              {isEmpty ? (
-                <div className="flex items-center justify-center">
-                  <img
-                    src="/static/images/empty.jpg"
-                    alt=""
-                    className="w-[700px]"
-                  />
-                </div>
-              ) : (
-                <MyCourseCard onIsEmptyChange={handleIsEmpty} />
-              )}
-            </div>
-          </div>
-        </div>
-        <CreateButton />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="bg-[#f2f2f2] h-screen">
+				<SideBar />
+				<Header
+					title={"MyCourse"}
+					description={
+						"Keep track on the save reviewers to keep learning and improving"
+					}
+				/>
+				<div className="ml-[210px]">
+					<div className="p-8">
+						<div className="pt-5">
+							{isEmpty ? (
+								<div className="flex items-center justify-center">
+									<img src="/static/images/empty.jpg" alt="" className="w-[700px]" />
+								</div>
+							) : (
+								<MyCourseCard onIsEmptyChange={handleIsEmpty} />
+							)}
+						</div>
+					</div>
+				</div>
+				<CreateButton />
+			</div>
+		</>
+	);
 };
