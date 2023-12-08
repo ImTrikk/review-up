ffa98eceafaa0fc16fcee077fccdaaaf9d6417a3
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-08 18:45:01 +0800

    updated the connection timeout

--- api/Database/database.js
+++ api/Database/database.js
@@ -2,21 +2,8 @@ import Pool from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-// localhost connections
-// export const dbConnection = new Pool.Pool({
-//  user: "postgres",
-//  password: "trikkgwapo123",
-//  host: "localhost",
-//  port: 5432,
-//  database: "ReviewupDB",
-// });
-
-// const { Pool } = require("pg");
-// require("dotenv").config(;)
-
 // for vercel postgre database
 export const dbConnection = new Pool.Pool({
 	connectionString: process.env.POSTGRE_DATABASE_CONNECTION_URL,
+	connectionTimeoutMillis: 60000,
 });
-
-
