1c37342263a72df97fa4d53f1fcec6bc993afd28
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-08 17:19:42 +0800

    update

--- api/index.js
+++ api/index.js
@@ -1,43 +1,49 @@
+// serverless.js
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { UserRouter } from "./Routes/Router.js";
 import { dbConnection } from "./Database/database.js";
-import { CourseRouter } from "./Routes/CourseRouters.js";
 import helmet from "helmet";
+import { CourseRouter } from "./Routes/CourseRouters.js";
 
 const app = express();
+
+// Middleware setup
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
+	cors({
+		origin: "*",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	}),
 );
 
-// sets Content Security Policy
+// Content Security Policy setup
 app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      scriptSrc: ["'self'", " https://review-up.vercel.app"],
-    },
-  })
+	helmet.contentSecurityPolicy({
+		directives: {
+			scriptSrc: ["'self'", "https://review-up.vercel.app"],
+		},
+	}),
 );
 
-app.use("/api/auth", UserRouter);
-app.use("/api/course", CourseRouter);
-
-// database connection
+// Ensure the database connection is established before starting the server
 dbConnection
-  .connect()
-  .then(() => {
-    console.log("Postgre connection success");
-  })
-  .catch((err) => {
-    console.log("Error connecting with vercel postgre DB");
-    console.log(err);
-  });
+	.connect()
+	.then(() => {
+		// Routes that depend on the database can be added here
+		app.use("/api/course", CourseRouter);
+		app.use("/api/auth", UserRouter);
 
-app.listen(4242, () => console.log("Server running on localhost 4242"));
+		// Start the server
+		app.listen(4242, () => {
+			console.log("Server running on localhost 4242");
+		});
+	})
+	.catch((err) => {
+		console.error("Error connecting with the PostgreSQL database");
+		console.error(err);
+		process.exit(1); // Exit the process with an error code
+	});
