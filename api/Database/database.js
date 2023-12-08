afb5da0ffab25d45248eb8496fe9d0f782e4e18e
ImTrikk <130761529+ImTrikk@users.noreply.github.com>
2023-12-08 18:49:47 +0800

    updated database

--- api/Database/database.js
+++ api/Database/database.js
@@ -6,4 +6,5 @@ dotenv.config();
 export const dbConnection = new Pool.Pool({
 	connectionString: process.env.POSTGRE_DATABASE_CONNECTION_URL,
 	connectionTimeoutMillis: 60000,
+	idleTimeoutMillis: 60000,
 });
